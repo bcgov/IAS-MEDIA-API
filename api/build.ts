acbb66f0101e99f7d495f535429cd6f5e7e02c36
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-05-30 17:14:05 -0700

    Added resources.

--- api/build.ts
+++ api/build.ts
@@ -2,9 +2,9 @@
  * Remove old files, copy front-end ones.
  */
 
-import fs from 'fs-extra';
+import * as fs from 'fs-extra';
 import Logger from 'jet-logger';
-import childProcess from 'child_process';
+import * as childProcess from 'child_process';
 
 // Setup logger
 const logger = new Logger();
@@ -15,7 +15,6 @@ logger.timestamp = false;
   try {
     // Remove current build
     await remove('./dist/');
-    await copy('./src/templates','./dist/src/templates');
     await exec('tsc --build tsconfig.prod.json', './');
   } catch (err) {
     logger.err(err);
@@ -30,15 +29,6 @@ function remove(loc: string): Promise<void> {
     });
   });
 }
-
-function copy(src: string, dest: string): Promise<void> {
-  return new Promise((res, rej) => {
-    return fs.copy(src, dest, (err) => {
-      return (!!err ? rej(err) : res());
-    });
-  });
-}
-
 
 function exec(cmd: string, loc: string): Promise<void> {
   return new Promise((res, rej) => {
